5d5cad28db8792e39c3c8646ec66f96788e44dfb
xCito <Juanlantiguajr@gmail.com>
2018-04-21 11:20:34 -0400

    Added send/receive function, fixed contact list, added animation for sending/receiving messages

--- response.js
+++ response.js
@@ -1,5 +1,17 @@
 var settingOpen = false;
 
+$(document).ready(function()
+{
+    chatInput = document.getElementById("chatInput");
+    chatInput.addEventListener("keyup", function(e)
+    {
+        e.preventDefault();
+        if(e.keyCode == 13)
+            sendMessage();
+    });
+})
+
+
 function displayHideSettings()
 {
     if(settingOpen)
@@ -19,5 +31,39 @@ function displayHideSettings()
         div.show();
         settingOpen = true;
         console.log("open");
+    }
+}
+
+function sendMessage()
+{
+    if($("#chatInput").val() != "")
+    {
+        var chat = document.getElementsByClassName("chatMessages")[0];
+        var field = document.getElementsByClassName("messageField")[0];
+        var msg= document.createElement("div");
+        msg.setAttribute("class", "chatBubbleSender");
+        msg.innerText = $("#chatInput").val();
+        $(msg).css("opacity", 0);
+        field.appendChild(msg);
+        $(chat).animate({scrollTop: $(chat).prop("scrollHeight")}, 500);
+        $(msg).animate({opacity: 1}, 500);
+        $("#chatInput").val("");
+    }
+}
+
+function receiveMessage()
+{
+    if($("#chatInput").val() != "")
+    {
+        var chat = document.getElementsByClassName("chatMessages")[0];
+        var field = document.getElementsByClassName("messageField")[0];
+        var msg= document.createElement("div");
+        msg.setAttribute("class", "chatBubbleRecipient");
+        msg.innerText = $("#chatInput").val();
+        $(msg).css("opacity", 0);
+        field.appendChild(msg);
+        $(chat).animate({scrollTop: $(chat).prop("scrollHeight")}, 500);
+        $(msg).animate({opacity: 1}, 500);
+        $("#chatInput").val("");
     }
 }
