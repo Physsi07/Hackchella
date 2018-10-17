c60e9cd877f8cd29ecd33c88e5f5e4a816c16c8a
physsi07 <starlinu33@gmail.com>
2018-10-17 00:02:36 -0400

    never pushed up to github, all files

---
+++ action.js
@@ -0,0 +1,24 @@
+
+// CODE THAT CHANGES FROM REGIESTER FORM TO LOGIN FORM //
+$(document).ready(function(){
+	    $('.login-form').hide();
+
+	    $("#register").click(function()
+	    {
+	    	$(".register-form").hide('slow');
+	    	$(".login-form").show('slow');
+	    });
+
+	    $(".login-reg-btn").click(function()
+	    {
+	    	$(".login-form").hide('slow');
+	    	$(".register-form").show('slow');
+	   	})
+});
+
+
+
+// var firstName  = document.getElementById("InputFirstName").value;
+// var secondName = document.getElementById("InputLastName").value;
+// var email      = document.getElementById("InputEmail").value;
+// var language   = document.getElementById("languuage").value;
